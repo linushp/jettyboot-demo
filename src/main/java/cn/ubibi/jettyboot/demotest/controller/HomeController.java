706d14bc35c6ba8e2d5ea3c76ae70f1cc8fcac0a
luan <master@ubibi.cn>
2018-04-23 09:32:31 +0800

    path路径支持多个

--- src/main/java/cn/ubibi/jettyboot/demotest/controller/HomeController.java
+++ src/main/java/cn/ubibi/jettyboot/demotest/controller/HomeController.java
@@ -4,11 +4,14 @@ import cn.ubibi.jettyboot.framework.rest.ControllerRequest;
 import cn.ubibi.jettyboot.framework.rest.annotation.Controller;
 import cn.ubibi.jettyboot.framework.rest.annotation.GetMapping;
 
+import java.util.Base64;
+
 @Controller({"/","/home","/index","welcome"})
 public class HomeController {
 
     @GetMapping({"/","index.html","index2.html"})
     public String helloHome(ControllerRequest request){
+
         return  "welcome home:" + request.getPathInfo() + "__" + request.getTargetPath();
     }
 }
