c5d482182d1060fb57f6b8828af08d3cee490445
luan <master@ubibi.cn>
2018-04-23 13:05:15 +0800

    CommunicationsException

--- src/main/java/cn/ubibi/jettyboot/demotest/components/MyClassPathResourceHandler.java
+++ src/main/java/cn/ubibi/jettyboot/demotest/components/MyClassPathResourceHandler.java
@@ -2,6 +2,12 @@ package cn.ubibi.jettyboot.demotest.components;
 
 import cn.ubibi.jettyboot.framework.rest.ResourceHandlers;
 import cn.ubibi.jettyboot.framework.rest.annotation.Component;
+import cn.ubibi.jettyboot.framework.rest.ifs.ResourceHandlerFilter;
+import org.eclipse.jetty.server.Request;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class MyClassPathResourceHandler extends ResourceHandlers.ContextClassPathResourceHandler {
