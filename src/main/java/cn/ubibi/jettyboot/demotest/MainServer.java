10bb83a977154bae94c6033aa256a39a10cf1c02
luan <master@ubibi.cn>
2018-05-03 09:32:28 +0800

    更加灵活的扩展

--- src/main/java/cn/ubibi/jettyboot/demotest/MainServer.java
+++ src/main/java/cn/ubibi/jettyboot/demotest/MainServer.java
@@ -2,15 +2,11 @@ package cn.ubibi.jettyboot.demotest;
 
 import cn.ubibi.jettyboot.demotest.controller.render.PageRender;
 import cn.ubibi.jettyboot.demotest.dao.base.MyConnectionFactory;
-
-import cn.ubibi.jettyboot.framework.commons.scan.PackageScanner;
 import cn.ubibi.jettyboot.framework.rest.JettyBootServer;
 
 
-import net.sf.cglib.proxy.Enhancer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 
 public class MainServer {
@@ -25,11 +21,9 @@ public class MainServer {
         PageRender.init();
 
 
-
         JettyBootServer server = new JettyBootServer();
         server.doScanPackage(MainServer.class);
         server.listen(8001);
-
     }
 
 }
---
+++ src/main/java/cn/ubibi/jettyboot/demotest/dao/MyUserDAO.java
@@ -0,0 +1,4 @@
+package cn.ubibi.jettyboot.demotest.dao;
+
+public class MyUserDAO extends UserDAO{
+}
