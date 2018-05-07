570d273eb9b9a849bf19fea059de6106a813575b
luan <master@ubibi.cn>
2018-05-07 16:41:39 +0800

    dataAccess支持类似mybatis那种SQL语法

--- src/main/java/cn/ubibi/jettyboot/demotest/controller/JsApiController.java
+++ src/main/java/cn/ubibi/jettyboot/demotest/controller/JsApiController.java
@@ -5,7 +5,10 @@ import cn.ubibi.jettyboot.framework.rest.ControllerMethodHandler;
 import cn.ubibi.jettyboot.framework.rest.ControllerContextHandler;
 import cn.ubibi.jettyboot.framework.rest.annotation.Controller;
 import cn.ubibi.jettyboot.framework.rest.annotation.GetMapping;
+import cn.ubibi.jettyboot.framework.rest.annotation.PathVariable;
+import cn.ubibi.jettyboot.framework.rest.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,46 +22,71 @@ public class JsApiController {
 
 
     @GetMapping("/")
-    public Object xxx(){
+    public Object xxx(@RequestParam("controller") String controller){
         List<ControllerMethodHandler> methods = context.getControllerMethodHandlers();
 
         String contextPath = context.getContextPath();
 
 
-        Map<String,String> map = new HashMap<>();
+        List<Api> apis = new ArrayList<>();
 
         for (ControllerMethodHandler methodHandler:methods){
-            methodHandler.getMethod();
+
 
             String methodName = methodHandler.getMethod().getName();
 
+            String url = methodHandler.getTargetPath();
+            if (!"/".equals(contextPath)){
+                url = contextPath + url;
+            }
 
-            methodHandler.getTargetPath();
-            methodHandler.getSupportRequestMethod();
-
-
-
-            String url = contextPath + methodHandler.getTargetPath();
-
-            String paramsString = "";
-
-            String functionCode = "function("+paramsString+"){" +
-                    " return ajax('"+methodHandler.getSupportRequestMethod()+"','"+url+"')" +
-                    "}";
-
-
-            map.put(methodName,functionCode);
-
+            if (controller.equals(methodHandler.getMethod().getDeclaringClass().getSimpleName())){
+                apis.add(new Api(methodHandler.getSupportRequestMethod(),url,methodName));
+            }
 
 
         }
 
+        return apis;
+    }
 
 
 
-        return map;
+    public static class Api{
+        private String method;
+        private String url;
+        private String func;
+
+        public Api(String annotationMethod, String url, String functionName) {
+            this.method = annotationMethod;
+            this.url = url;
+            this.func = functionName;
+        }
 
 
+        public String getMethod() {
+            return method;
+        }
+
+        public void setMethod(String method) {
+            this.method = method;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getFunc() {
+            return func;
+        }
+
+        public void setFunc(String func) {
+            this.func = func;
+        }
     }
 }
 
