af49268ddc59a7874d12022e38577be8a0d99e6b
luan <master@ubibi.cn>
2018-05-12 13:00:50 +0800

    DWR支持任意数组形式的参数

--- src/main/java/cn/ubibi/jettyboot/demotest/controller/DwrTestController.java
+++ src/main/java/cn/ubibi/jettyboot/demotest/controller/DwrTestController.java
@@ -1,19 +1,39 @@
 package cn.ubibi.jettyboot.demotest.controller;
 
 
+import cn.ubibi.jettyboot.demotest.dao.UserDAO;
+import cn.ubibi.jettyboot.demotest.entity.UserEntity;
+import cn.ubibi.jettyboot.framework.ioc.Autowired;
 import cn.ubibi.jettyboot.framework.rest.annotation.DwrController;
 import cn.ubibi.jettyboot.framework.rest.annotation.DwrFunction;
 
 @DwrController
 public class DwrTestController {
 
+    @Autowired
+    private UserDAO userDAO;
+
     @DwrFunction
-    public int add(int a, int b) {
-        return a + b;
+    public UserEntity findById(int id) throws Exception {
+        return userDAO.findById(id);
+    }
+
+
+    @DwrFunction
+    public int add(int [][] arr) {
+        int result = 0;
+        for (int i = 0 ; i < arr.length ; i++){
+            int [] arr2 = arr[i];
+            for (int j=0;j<arr2.length ;j++){
+                int a = arr2[j];
+                result=result + a;
+            }
+        }
+        return result;
     }
 
     @DwrFunction
-    public int minus(int a, int b) {
+    public int minus(Integer a, char b) {
         return a - b;
     }
 
