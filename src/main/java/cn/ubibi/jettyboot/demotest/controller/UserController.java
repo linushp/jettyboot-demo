451daa89cf41f222f0dc5e0d2ec4106d6f0e047d
luan <master@ubibi.cn>
2018-05-14 11:27:14 +0800

    toBasicTypeOf函数名变更

--- src/main/java/cn/ubibi/jettyboot/demotest/controller/UserController.java
+++ src/main/java/cn/ubibi/jettyboot/demotest/controller/UserController.java
@@ -29,7 +29,7 @@ import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
 
 
-@Controller({"/user","/vip"})
+@Controller({"/user", "/vip"})
 public class UserController {
 
     private static Logger logger = Log.getLogger(UserController.class);
@@ -39,98 +39,93 @@ public class UserController {
 
 
     @GetMapping("/helloworld/:name")
-    public String helloworld(ControllerRequest request,@PathVariable("name") String name){
+    public String helloworld(ControllerRequest request, @PathVariable("name") String name) {
         return "hello world : vip " + isVip(request) + " name " + name;
     }
 
 
-    private boolean isVip(ControllerRequest request){
+    private boolean isVip(ControllerRequest request) {
         String target = request.getTargetPath();
-        if (target.startsWith("/vip")){
+        if (target.startsWith("/vip")) {
             return true;
         }
         return false;
     }
 
 
-
     @GetMapping("/test_page")
-    public PageRender getmm212_page(@RequestParam("name") String name,@AspectVariable CurrentUser currentUser) throws Exception {
-        Map<String,String> map = new HashMap<>();
-        map.put("name",name + "__" + this.getClass().getClassLoader().getClass().getName() + "___" + currentUser.getName());
-        return new PageRender("test.html",map);
+    public PageRender getmm212_page(@RequestParam("name") String name, @AspectVariable CurrentUser currentUser) throws Exception {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", name + "__" + this.getClass().getClassLoader().getClass().getName() + "___" + currentUser.getName());
+        return new PageRender("test.html", map);
     }
-
 
 
     @GetMapping("/test_insert2")
     public UpdateResult getmm212(@RequestParam("name") String name) throws Exception {
         Map<String, Object> map = new HashMap<>();
-        map.put("name",name);
-        map.put("yaoli",123);
-        map.put("dai",3);
-        map.put("fid",3);
-        map.put("mid",3);
-        map.put("create_time",System.currentTimeMillis());
-        map.put("update_time",System.currentTimeMillis());
+        map.put("name", name);
+        map.put("yaoli", 123);
+        map.put("dai", 3);
+        map.put("fid", 3);
+        map.put("mid", 3);
+        map.put("create_time", System.currentTimeMillis());
+        map.put("update_time", System.currentTimeMillis());
         return userDAO.insertObject(map);
     }
-
 
 
     @GetMapping("/test_insert")
     public UpdateResult getmm21(@RequestParams UserInfoParser reqParser) throws Exception {
         Map<String, Object> map = new HashMap<>();
-        map.put("name","name" + System.currentTimeMillis() + "_" + Math.random());
-        map.put("yaoli",123);
-        map.put("dai",3);
-        map.put("fid",3);
-        map.put("mid",3);
-        map.put("create_time",System.currentTimeMillis());
-        map.put("update_time",System.currentTimeMillis());
-       return userDAO.insertObject(map);
+        map.put("name", "name" + System.currentTimeMillis() + "_" + Math.random());
+        map.put("yaoli", 123);
+        map.put("dai", 3);
+        map.put("fid", 3);
+        map.put("mid", 3);
+        map.put("create_time", System.currentTimeMillis());
+        map.put("update_time", System.currentTimeMillis());
+        return userDAO.insertObject(map);
     }
 
 
-
-    @GetMapping( "/test")
+    @GetMapping("/test")
     public String getmm(UserInfoParser reqParser, ControllerRequest request, CurrentUser currentUser) throws Exception {
         new UserDAO().findAll();
-        if(reqParser instanceof RequestParser){
+        if (reqParser instanceof RequestParser) {
             System.out.println("111");
         }
-        return  "123---" + reqParser.getName() +"=====" + currentUser.getName();
+        return "123---" + reqParser.getName() + "=====" + currentUser.getName();
     }
-
-
 
 
     @GetMapping(value = "/")
-    public Page<UserEntity> getUserById3(ControllerRequest JBRequest, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public Page<UserEntity> getUserById3(
+            @RequestParam(value = "pageSize") int pageSize,
+            @RequestParam(value = "pageNo") int pageNo,
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        Integer pageSize = JBRequest.getRequestParam("pageSize","10").toInteger();
-        Integer pageNo = JBRequest.getRequestParam("pageNo","0").toInteger();
+
 
         long t1 = System.currentTimeMillis();
         Page<UserEntity> result = userDAO.findPage(pageNo, pageSize);
 
         long t2 = System.currentTimeMillis();
 
-        logger.info("::"+(t2-t1));
+        logger.info("::" + (t2 - t1));
         return result;
     }
-
-
 
 
     @GetMapping(value = "/:uid")
     public Object getUserById(ControllerRequest params, HttpServletResponse response) throws Exception {
 
-        String uid =  params.getPathVariable("uid").toString();
-        String name = params.getRequestParam("name").toString();
+        String uid = params.getPathVariable("uid");
+        String name = params.getRequestParam("name", "");
+
         String[] names = params.getParameterValues("name");
 
-        Cookie cookie = new Cookie("aaa","a2333");
+        Cookie cookie = new Cookie("aaa", "a2333");
 
         response.addCookie(cookie);
 
@@ -146,18 +141,17 @@ public class UserController {
 
 
     @DwrFunction
-    public String getUserInfoById(Object a, Integer b, @AspectVariable CurrentUser currentUser){
+    public String getUserInfoById(Object a, Integer b, @AspectVariable CurrentUser currentUser) {
         // UserController.getUserInfoById(123);
-        return "getUserInfoById:  " + (a.toString()+b);
+        return "getUserInfoById:  " + (a.toString() + b);
     }
 
 
     @DwrFunction
-    public String getUserInfoByName(String name, @AspectVariable CurrentUser currentUser){
+    public String getUserInfoByName(String name, @AspectVariable CurrentUser currentUser) {
         // UserController.getUserInfoById(123);
         return "getUserInfoByName:  " + name;
     }
-
 
 
 }
