87d019316489ebaf1dc4cecdedba9bf7b1212ac8
luan <master@ubibi.cn>
2018-06-05 17:24:05 +0800

    用户可自定义ResultSetParser

--- src/main/java/cn/ubibi/jettyboot/demotest/dao/base/MyDAO2.java
+++ src/main/java/cn/ubibi/jettyboot/demotest/dao/base/MyDAO2.java
@@ -5,10 +5,15 @@ import cn.ubibi.jettyboot.framework.jdbc.DataAccessAbstract;
 import cn.ubibi.jettyboot.framework.jdbc.DataAccessObject;
 
 public abstract class MyDAO2<T> extends DataAccessAbstract<T> {
-    private DataAccessObject<T> dataAccessObject;
+
 
     public MyDAO2() {
         super(MyConnectionFactory.getInstance());
+    }
+
+    public void insert(String tableName, T entity) throws Exception {
+        DataAccessObject<T> dataAccessObject = new DataAccessObject<>(null, tableName, MyConnectionFactory.getInstance());
+        dataAccessObject.insertObject(entity);
     }
 
 }
