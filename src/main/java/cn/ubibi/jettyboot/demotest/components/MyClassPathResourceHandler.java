e07b88e0190b8f1728389fa8cd31ec65cc66414d
luanhaipeng <luanhaipeng@staff.beechat.io>
2018-08-17 13:55:44 +0800

    thread safe

--- src/main/java/cn/ubibi/jettyboot/demotest/components/MyClassPathResourceHandler.java
+++ src/main/java/cn/ubibi/jettyboot/demotest/components/MyClassPathResourceHandler.java
@@ -1,16 +1,10 @@
 package cn.ubibi.jettyboot.demotest.components;
 
 import cn.ubibi.jettyboot.demotest.configs.SystemConfig;
-import cn.ubibi.jettyboot.framework.rest.ResourceHandlers;
+import cn.ubibi.jettyboot.framework.rest.handlers.ResourceHandlers;
 import cn.ubibi.jettyboot.framework.rest.annotation.Component;
 import cn.ubibi.jettyboot.framework.rest.annotation.ComponentFactory;
-import cn.ubibi.jettyboot.framework.rest.ifs.ResourceHandlerFilter;
-import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.ContextHandler;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 @ComponentFactory
